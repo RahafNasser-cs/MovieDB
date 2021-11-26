96c0a4ef34856cff10b696b41107c46bd46d2105
Rahaf Nasser <rahaf.nasser.ruh.k@tuwaiq.edu.sa>
2021-11-26 18:19:21 +0300

    implement MovieViewModel

--- app/src/main/java/com/example/moviedb/model/MovieViewModel.kt
+++ app/src/main/java/com/example/moviedb/model/MovieViewModel.kt
@@ -1,5 +1,27 @@
 package com.example.moviedb.model
 
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.moviedb.network.MovieInfoResponse
+import kotlinx.coroutines.launch
+import java.lang.Exception
 
-class MovieViewModel : ViewModel()
+class MovieViewModel : ViewModel() {
+    private var _movie = MutableLiveData<MovieInfoResponse>()
+    val movie: MutableLiveData<MovieInfoResponse> get() = _movie
+
+    init {
+    }
+
+    private fun getMovie() {
+        viewModelScope.launch {
+            try {
+                _movie.value = MovieApi.retrofitService.getInfo()
+            } catch (e: Exception) {
+                Log.d("viewModelScope", "error --> $e")
+            }
+        }
+    }
+}
