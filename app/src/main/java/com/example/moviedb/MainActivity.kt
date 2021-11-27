7f161b7ba1fb3b3e17dabdb0acdb769b9e946ea7
Rahaf Nasser <rahaf.nasser.ruh.k@tuwaiq.edu.sa>
2021-11-28 01:16:27 +0300

    complete favourite feature

--- app/src/main/java/com/example/moviedb/MainActivity.kt
+++ app/src/main/java/com/example/moviedb/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.example.moviedb
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     lateinit var navController: NavController
@@ -15,9 +20,20 @@ class MainActivity : AppCompatActivity() {
             supportFragmentManager.findFragmentById(R.id.nav_host) as NavHostFragment
         navController = navHostFragment.navController
         setupActionBarWithNavController(navController)
+
+        val buttonNavigationView = findViewById<BottomNavigationView>(R.id.activity_main_bottom_navigation_view)
+        buttonNavigationView.setupWithNavController(navController)
     }
 
     override fun onSupportNavigateUp(): Boolean {
         return super.onSupportNavigateUp() || navController.navigateUp()
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.favorite_page -> {
+                navController.navigate(R.id.action_homeFragment_to_favouriteFragment)
+            }
+        }
+        return true
     }
 }
--- app/src/main/java/com/example/moviedb/movie/HomeFragment.kt
+++ app/src/main/java/com/example/moviedb/movie/HomeFragment.kt
@@ -1,17 +1,24 @@
 package com.example.moviedb.movie
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.activity.addCallback
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import com.example.moviedb.R
 import com.example.moviedb.databinding.FragmentHomeBinding
 import com.example.moviedb.model.MovieViewModel
+import com.example.moviedb.network.ResultsItem
 
 class HomeFragment : Fragment() {
     private val viewModel: MovieViewModel by viewModels()
+    lateinit var binding: FragmentHomeBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,18 +30,44 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val binding = FragmentHomeBinding.inflate(inflater)
+        binding = FragmentHomeBinding.inflate(inflater)
         // Allows Data Binding to Observe LiveData with the lifecycle of this Fragment
         binding.lifecycleOwner = this
-        binding.recyclerView.adapter = MovieGridAdapter()
+        binding.recyclerView.adapter = MovieGridAdapter("home")
 
         // Giving the binding access to the OverviewViewModel
         binding.viewModel = viewModel
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        requireActivity().onBackPressedDispatcher.addCallback {
+            findNavController().navigateUp()
+        }
+    }
+
     fun goToNextFragment() {
 //        findNavController().navigate(R.id.action_homeFragment_to_detailsFragment)
         Toast.makeText(requireContext(), "goToNextFragment()", Toast.LENGTH_LONG).show()
+    }
+
+    fun addFavMovie(movie: ResultsItem) {
+        Log.d("Home Frag addFavMovie()  ", "oooooo")
+        viewModel.addFavMovie(movie)
+        Log.d("Home Frag addFavMovie()  ", "${viewModel.favMovies.value!!}")
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.favorite_page -> {
+                findNavController().navigate(R.id.action_homeFragment_to_favouriteFragment)
+            }
+        }
+        return true
+    }
+
+    fun test() {
+        Log.d("Home Frag test()  ", "oooooo")
     }
 }
